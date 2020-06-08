cf67bf2663271a77afaca3b430eef09085c67795
rchytas <mcpandit@gmail.com>
2020-06-08 15:55:38 -0700

    fixed not importing when running the app

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.secret_key = 'IAcceptTheRisk'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Parent, '/parent/<string:name>')
